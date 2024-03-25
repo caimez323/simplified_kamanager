5366ea810617c891c58503f6bafa7d147ca7f386
caimez323 <caimezae@gmail.com>
2024-03-25 19:09:50 +0100

    add gears id

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,12 +2,15 @@ import re
 
 count = 0
 replaced_lines = []
-with open("./resources.resources.json","r",encoding="utf-8") as file:
+nextIsId = True
+with open("./resources.gears.json","r",encoding="utf-8") as file:
     lines = file.readlines()
     for line in lines:
-        if "id" in line:
+        if "quantity" in line:
+            nextIsId = False
+        if "X_X_X" in line:
             count+=1
         replaced_lines.append(line.replace("X_X_X","{},".format(count)))
 
-with open("./resources.resources.json","w",encoding="utf-8") as file:
+with open("./resources.gears.json","w",encoding="utf-8") as file:
     [file.write(line) for line in replaced_lines]
