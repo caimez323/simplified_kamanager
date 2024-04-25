34aece5ef6fd30883bc6869acfa7694a1444d239
Clement Thiebault <clement.thiebault@dolphin.fr>
2024-04-25 12:21:55 +0200

    commit before upload of new resources database

--- opti.py
+++ opti.py
@@ -1,14 +1,28 @@
 import json
 
-replaced_data = []
+replaced_data = {}
+idUsed = []
+idToRemove = []
 count = 0
-with open("resources.gears.json",encoding="utf-8") as gearsFile:
-    gearsData = json.load(gearsFile)
-    for gear in gearsData:
-        newGear = gear
-        newGear["id"] = count+1
-        replaced_data.append(newGear)
-        count+=1
 
-with open("datatest.json","w",encoding="utf-8") as file:
+with open("gears_format.json",encoding="utf-8") as gearsFile:
+    gearsData = json.load(gearsFile)
+    
+    for id,res in gearsData.items():
+        recipe = res["recipe"]
+        for compo in recipe:
+            idUsed.append(compo["id"])
+        
+with open("resources_format.json",encoding="utf-8") as resourcesFile:
+    resourcesData = json.load(resourcesFile)
+    
+    for id,res in resourcesData.items():
+        if id not in idUsed:
+            print("Will remove {}".format(id))
+        else:
+            replaced_data[id]=res
+
+
+with open("resources_format.json","w",encoding="utf-8") as file:
     json.dump(replaced_data, file, ensure_ascii=False, indent=4)
+
